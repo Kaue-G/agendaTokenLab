338292c4e298419aed8eeaa04b8feca354778441
Kaue-G <kaueguedes.dev@gmail.com>
2022-07-23 12:07:05 -0300

    initial setting

---
+++ backEnd/src/shared/infra/http/server.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import swaggerUI from 'swagger-ui-express';
+import swaggerFile from '../../../swagger.json';
+import { router } from "./routes/index.routes";
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerFile));
+
+app.use(router);
+
+app.listen(3333, () => console.log("server on"));
