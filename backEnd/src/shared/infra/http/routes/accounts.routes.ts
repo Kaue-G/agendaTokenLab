965c263dd3f3dccd1ae85d5163d43eff0d6cee0e
Kaue-G <kaueguedes.dev@gmail.com>
2022-07-24 13:37:31 -0300

    Change Password

--- backEnd/src/shared/infra/http/routes/accounts.routes.ts
+++ backEnd/src/shared/infra/http/routes/accounts.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { ChangePasswordController } from "../../../../modules/accounts/useCases/changePassword/changePasswordController";
 import { CreateUserController } from "../../../../modules/accounts/useCases/createUser/createUserController";
 import { DeleteUserController } from "../../../../modules/accounts/useCases/deleteUser/deleteUserController";
 import { ListUserController } from "../../../../modules/accounts/useCases/listUsers/listUsersController";
@@ -10,10 +11,12 @@ const createUserController = new CreateUserController();
 const listUserController = new ListUserController();
 const deleteUserController = new DeleteUserController();
 const updateUserController = new UpdateUserController();
+const changePasswordController = new ChangePasswordController();
 
 accountsRoutes.post('/', createUserController.handle);
 accountsRoutes.get('/', listUserController.handle);
 accountsRoutes.delete('/', deleteUserController.handle);
 accountsRoutes.put('/', updateUserController.handle);
+accountsRoutes.patch('/changePassword', changePasswordController.handle);
 
 export { accountsRoutes };
