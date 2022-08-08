5c705b3254f4a91d5122e364c3f9965c51851046
Kaue-G <kaueguedes.dev@gmail.com>
2022-08-08 14:05:13 -0300

    hotFix

--- backEnd/src/modules/events/useCases/createEvent/createEventController.ts
+++ backEnd/src/modules/events/useCases/createEvent/createEventController.ts
@@ -5,16 +5,16 @@ import { CreateEventUseCase } from './createEventUseCase';
 class CreateEventController {
     async handle(request: Request, response: Response): Promise<Response> {
         const { 
-            description, 
-            user_id,
+            description,
             days
         } = request.body;
+        const { id } = request.user;
 
         const createEventUseCase = container.resolve(CreateEventUseCase);
         
         await createEventUseCase.execute({
             description, 
-            user_id,
+            user_id: id,
             days
         });
         return response.status(201).send();
