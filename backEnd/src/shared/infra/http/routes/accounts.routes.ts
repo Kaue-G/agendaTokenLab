2d2c8fd27edbdf8f30e27e27d6c4a4b2de59be60
Kaue-G <kaueguedes.dev@gmail.com>
2022-08-08 12:59:32 -0300

    routes

--- backEnd/src/shared/infra/http/routes/accounts.routes.ts
+++ backEnd/src/shared/infra/http/routes/accounts.routes.ts
@@ -3,7 +3,6 @@ import { ChangePasswordController } from "../../../../modules/accounts/useCases/
 import { CreateUserController } from "../../../../modules/accounts/useCases/createUser/createUserController";
 import { DeleteUserController } from "../../../../modules/accounts/useCases/deleteUser/deleteUserController";
 import { ListUserController } from "../../../../modules/accounts/useCases/listUsers/listUsersController";
-import { AuthenticateUserController } from "../../../../modules/accounts/useCases/authenticateUser/AuthenticateUserController";
 import { UpdateUserController } from "../../../../modules/accounts/useCases/updateUser/updateUserController";
 
 const accountsRoutes = Router();
@@ -13,10 +12,8 @@ const listUserController = new ListUserController();
 const deleteUserController = new DeleteUserController();
 const updateUserController = new UpdateUserController();
 const changePasswordController = new ChangePasswordController();
-const authenticateUserController = new AuthenticateUserController();
 
 accountsRoutes.post('/', createUserController.handle);
-accountsRoutes.post('/login', authenticateUserController.handle);
 accountsRoutes.get('/', listUserController.handle);
 accountsRoutes.delete('/', deleteUserController.handle);
 accountsRoutes.put('/', updateUserController.handle);
--- backEnd/src/shared/infra/http/routes/index.routes.ts
+++ backEnd/src/shared/infra/http/routes/index.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { accountsRoutes } from "./accounts.routes";
+import { authenticationRoutes } from "./authenticate.routes";
 import { eventsRoutes } from "./events.routes";
 
 const router = Router();
 
+router.use(authenticationRoutes);
 router.use('/accounts', accountsRoutes);
 router.use('/events', eventsRoutes);
 
